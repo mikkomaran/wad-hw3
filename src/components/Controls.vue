a279de98f65cdac266531f41393f37c8b9106a89
Mikko Maran <mixxo.maran@gmail.com>
2019-11-06 18:52:07 +0200

    Made 5 smaller components from App.vue

---
+++ src/components/Controls.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="controls">
+        <button id="profile-button" class="pill active">Profile</button>
+        <button id="courses-button" class="pill">Courses</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Controls"
+    }
+</script>
+
+<style scoped>
+
+    .controls .pill {
+        border: 1px solid #cbcbcb;
+        background-color: #eaeaea;
+        padding: 10px;
+        border-bottom-left-radius: 4px;
+        border-bottom-right-radius: 4px;
+        border-top: none;
+        margin-top: -1px;
+        outline: none !important;
+    }
+
+    .controls .pill.active {
+        background-color: #ffffff;
+        border-top: 1px solid #ffffff;
+    }
+
+    .controls .pill:hover {
+        cursor: pointer;
+        background-color: #ffffff;
+    }
+
+</style>
